73620bd810cdae439bc702dc7994d6f4b1983b36
Théo Garde <theogarde@Ordinateur-portable-de-Theo.local>
2026-05-27 14:18:04 +0200

    fix

--- Frontend/portfolio/next.config.ts
+++ Frontend/portfolio/next.config.ts
@@ -1,7 +1,18 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
+
+const nextConfig: NextConfig = {
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  // Remplace par le nom EXACT de ton repo GitHub
+  basePath: '/portfolio-EPITECH',
+  assetPrefix: '/portfolio-EPITECH/',
+};
+
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
